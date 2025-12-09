e1ebe6dbc1db9cc79fc04ef0904546e5738e00d3
Jacques Thurling <jthurling78@gmail.com>
2025-12-09 09:25:47 -0800

    Added more

--- lua/plugins/gruv.lua
+++ lua/plugins/gruv.lua
@@ -1,13 +1,14 @@
 return {
-	{ "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
-	-- Using Lazy
-	{
-		"navarasu/onedark.nvim",
-		priority = 1000, -- make sure to load this before all the other start plugins
-		config = function()
-			require("onedark").setup({
-				style = "darker",
-			})
-		end,
-	},
+  { "ellisonleao/gruvbox.nvim", priority = 1000, config = true },
+  -- Using Lazy
+  {
+    "navarasu/onedark.nvim",
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      require('onedark').setup {
+        style = 'darker'
+      }
+      require('onedark').load()
+    end
+  }
 }
